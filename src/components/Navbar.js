bc38731ece0ccd89defcde4d03143baecd17c4b6
Kshitij-Raj-01 <kshwebsites@gmail.com>
2024-06-19 12:34:37 +0530

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/Kshitij-Raj-01"
+                href="https://github.com/Kshitij-Raj-01/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
